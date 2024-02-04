6e3677faeb51820325b78e306dceb1b8ddfb229e
Deke Kraft <deke.kraft@gmail.com>
2024-02-03 20:43:45 -0800

    Cleaned up config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,8 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use dirs;
-use ini;
+use ini::{Ini, Properties};
+use log::info;
+use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct Config {
@@ -19,33 +21,63 @@ impl Config {
     }
 
     pub fn load() -> Result<Self> {
-        let config_file = Config::config_file_name().unwrap();
-        print!("CONFIG FILE: {:?}", config_file);
-        let config = ini::Ini::load_from_file(config_file).unwrap();
-        let section = config.general_section();
-        let device_id = section.get("device_id").unwrap();
-        let execute_on_connect = section.get("execute_on_connect").unwrap();
-        let execute_on_disconnect = section.get("execute_on_disconnect").unwrap();
-        let config = Config::new(
-            device_id.to_string(),
-            execute_on_connect.to_string(),
-            execute_on_disconnect.to_string(),
-        );
-        println!("{:?}", config);
-        // Ok(Self::new(
-        //     "2109:0103".to_string(),
-        //     "echo connected".to_string(),
-        //     "echo disconnected".to_string(),
-        // ))
+        let config_file = Config::config_file_name()?;
+        info!("Loading config from: {:?}", config_file);
+
+        let ini = Ini::load_from_file(config_file)?;
+        let config = Config::build_config(ini)?;
+        info!("Loaded config: {:?}", config);
+
         Ok(config)
     }
 
-    pub fn config_file_name() -> Result<std::path::PathBuf> {
-        let config_dir = dirs::config_dir()
-            .ok_or_else(|| "Config directory not found")
-            .unwrap()
-            .join("dev.deke.usb-commands/usb-commands.ini");
+    fn build_config(ini: Ini) -> Result<Config> {
+        let section = ini.general_section();
+        let device_id = Config::extract_value(section, "device_id")?;
+        let execute_on_connect = Config::extract_value(section, "execute_on_connect")?;
+        let execute_on_disconnect = Config::extract_value(section, "execute_on_disconnect")?;
+        let config = Config::new(device_id, execute_on_connect, execute_on_disconnect);
 
-        Ok(config_dir)
+        Ok(config)
+    }
+
+    fn extract_value(section: &Properties, key: &str) -> Result<String> {
+        section
+            .get(key)
+            .map(|s| s.to_string())
+            .ok_or_else(|| anyhow!("{} not found in config file", key))
+    }
+
+    pub fn config_file_name() -> Result<PathBuf> {
+        let config_dir = dirs::config_dir().ok_or_else(|| anyhow!("Config directory not found"))?;
+
+        Ok(config_dir.join("dev.deke.usb-commands/usb-commands.ini"))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config() {
+        let input = r#"
+        device_id = "0h1d:0a0a"
+        execute_on_connect = "/usr/local/bin/m1ddc set input 15"
+        execute_on_disconnect = "/usr/local/bin/m1ddc set input 17"
+        "#;
+
+        let ini = Ini::load_from_str(input).unwrap();
+        let config = Config::build_config(ini).unwrap();
+
+        assert_eq!(config.device_id, "0h1d:0a0a");
+        assert_eq!(
+            config.execute_on_connect,
+            "/usr/local/bin/m1ddc set input 15"
+        );
+        assert_eq!(
+            config.execute_on_disconnect,
+            "/usr/local/bin/m1ddc set input 17"
+        );
     }
 }
