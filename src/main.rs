0bc18e7f917d743ef8b48034d90b88288701935f
Deke Kraft <deke.kraft@gmail.com>
2024-02-04 09:05:35 -0800

    Added env_logger

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ mod config;
 mod device_id;
 mod listener;
 use anyhow::Result;
+use log::info;
 
 fn main() -> Result<()> {
+    env_logger::init();
+    info!("Starting usb-commands");
     let config = config::Config::load()?;
     listener::start_listener(config)?;
 
